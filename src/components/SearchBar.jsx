5e5e142b57cf28957eeb692f8ed1b07864314745
unknown <nietorosse7@gmail.com>
2024-04-16 15:40:52 -0700

    changes searchbar components

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,8 +1,29 @@
-function SearchBar(){
+import {useState} from "react"
+
+function searchBar(onSubmit){
+    const [term,setTerm]=useState('')
+    const handleFromSubmit=(event)=>{
+        event.preventDefault()
+        console.log('Necesito decirle al componente padre sobre los componentes')
+        onsubmit(term)
+    }
+        const handleChange = (event)=>{
+            console.log(event.target.value)
+            setTerm(event.target.value)
+        }
+    const handleClick = () =>{
+        onsubmit(term)
+    }
+
     return(
     <div>
-        <h1>images: </h1>
-    </div>
+        <h1>Search Bar: </h1>
+        <form onSubmit={handleFormSubmit}>
+            confirma tu <busqueda:>term</busqueda:>
+            <input onChange={(handleChange)} value={term}/>
+            </form>
+            <button onClick={handleClick}>Click me </button> 
+        </div>
     )
 }
 export default SearchBar
