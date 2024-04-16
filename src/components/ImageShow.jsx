dedc2627d782eaff4900f411b40ae17e4eca7e56
unknown <nietorosse7@gmail.com>
2024-04-16 15:04:43 -0700

    vreate imagelist, imageshow, SearchBar components

---
+++ src/components/ImageShow.jsx
@@ -0,0 +1,9 @@
+function ImageShow(){
+    return(
+        <div>
+            <h1>Image: </h1>
+        </div>
+    )
+
+}
+export default ImageShow
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,8 @@
+function SearchBar(){
+    return(
+    <div>
+        <h1>images: </h1>
+    </div>
+    )
+}
+export default SearchBar
